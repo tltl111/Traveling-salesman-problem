151db4e662886050c4a31ba5f691c1d4193e7ee0
tltl111 <tlafeber@outlook.com>
2023-05-26 15:21:42 +0200

    Shortest route finder.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         
-        nodeList nodeList = new nodeList();
-        Node leeuwarden = new Node("leeuwarden",53.201390,5.808590);
-        Node utrecht = new Node("utrecht", 52.090830,5.122220);
-        Node groningen = new Node("groningen",53.219170,6.566670);
-        Node arnhem = new Node("arnhem", 51.980000,5.911110);
-        Node maastricht = new Node("maastricht", 50.848330,5.688890);
-        Node assen = new Node("assen",52.996670,6.562500);
-        Node lelystad = new Node("lelystad", 52.508330,5.475000);
-        Node amsterdam = new Node("amsterdam", 52.374030,4.889690);
-        Node denhaag = new Node("den haag", 52.076670,4.298610);
-        Node middelburg = new Node("middelburg", 51.500000,3.613890);
-        Node shertogenbosch = new Node("'s hertogenbosch", 51.699170,5.304170);
+        NodeList nodeList = new NodeList();
+        Node leeuwarden = new Node("Leeuwarden",53.201390,5.808590);
+        Node utrecht = new Node("Utrecht", 52.090830,5.122220);
+        Node groningen = new Node("Groningen",53.219170,6.566670);
+        Node arnhem = new Node("Arnhem", 51.980000,5.911110);
+        Node maastricht = new Node("Maastricht", 50.848330,5.688890);
+        Node assen = new Node("Assen",52.996670,6.562500);
+        Node lelystad = new Node("Lelystad", 52.508330,5.475000);
+        Node amsterdam = new Node("Amsterdam", 52.374030,4.889690);
+        Node denhaag = new Node("Den haag", 52.076670,4.298610);
+        Node middelburg = new Node("Middelburg", 51.500000,3.613890);
+        Node shertogenbosch = new Node("'s Hertogenbosch", 51.699170,5.304170);
 
         nodeList.addNode(leeuwarden);
         nodeList.addNode(utrecht);
@@ -26,7 +26,7 @@ public class Main {
         nodeList.addNode(middelburg);
         nodeList.addNode(shertogenbosch);
 
-        nodeList.printNodes();
+        nodeList.solve();
 
     }
 }
--- src/nodeList.java
+++ src/nodeList.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
+import java.util.Random;
 
-public class nodeList {
+
+public class NodeList {
     private List<Node> nodes;
 
-    public nodeList() {
+    public NodeList() {
         this.nodes = new ArrayList<>();
     }
 
@@ -20,6 +23,58 @@ public class nodeList {
         List<Node> nodes = getNodes();
         for (Node node : nodes) {
             System.out.println(node.getName() + " (" + node.getXPos() + ", " + node.getYPos() + ")");
+        }
+    }
+
+    public void printOneNode() {
+        List<Node> nodes = getNodes();
+        Node node1 = nodes.get(0);
+        String printName = node1.getName();
+        System.out.println(printName);
+    }
+
+    public double getDistance(Node node1, Node node2) {
+        double differenceSqaureX = Math.pow(node1.getXPos() - node2.getXPos(), 2);
+        double differenceSqaureY = Math.pow(node1.getYPos() - node2.getYPos(), 2);
+        return Math.sqrt(differenceSqaureX + differenceSqaureY);
+    }
+
+    public void printDistance(Node node1, Node node2) {
+        double distance = getDistance(node1, node2);
+        System.out.println("The distance between " + node1.getName() + " and " + node2.getName() + " is: " + distance);
+    }
+
+    public void solve() {
+        Random random = new Random();
+        List<Node> newRoute = new ArrayList<>();
+        List<Node> nodes = getNodes();
+        
+        int startNode = random.nextInt(nodes.size());
+        // int startNode = 0;
+        newRoute.add(nodes.get(startNode));
+        nodes.remove(startNode);
+
+        while (!nodes.isEmpty()) {
+            Node lastNode = newRoute.get(newRoute.size() - 1);
+            Node closestNode = null;
+            double shortestDistance = Double.MAX_VALUE;
+
+            for (Node node : nodes) {
+                double distance = getDistance(lastNode, node);
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    closestNode = node;
+                }
+            }
+
+            if (closestNode != null) {
+                newRoute.add(closestNode);
+                nodes.remove(closestNode);
+            }
+        }
+        System.out.println("Route:\n");
+        for (Node node : newRoute) {
+            System.out.println(node.getName());
         }
     }
 }
